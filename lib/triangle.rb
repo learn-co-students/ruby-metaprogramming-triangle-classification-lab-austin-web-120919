258a42c64da60301b5432ab82703481bb70ef812
Chance Mims <chance@Chances-MacBook-Air.local>
2019-12-30 10:10:59 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Triangle
   # write code here
+
+  attr_reader :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if side_a <= 0 || side_b <= 0 || side_c <= 0
+      raise TriangleError
+    elsif side_a + side_b <= side_c || side_a + side_c <= side_b || side_b + side_c <= side_a
+      raise TriangleError
+    elsif side_a == side_b && side_a == side_c
+      return :equilateral
+    elsif side_a == side_b || side_a == side_c || side_b == side_c
+      return :isosceles
+    else return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+    def side_equals_zero
+      "Side cannot be equal to zero"
+    end
+
+    def triangle_inequality
+      "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
+    end
+
+  end
+
 end
